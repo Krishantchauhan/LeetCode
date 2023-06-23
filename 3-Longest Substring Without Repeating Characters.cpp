d6a21e9be05d27bae1feaed03b07c052fccab479
Krishantchauhan <krashantchauhan@gmail.com>
2023-06-23 19:14:58 +0530

    3. Longest Substring Without Repeating Characters
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 3-Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char,int> mp;
+
+        int maxi = 1;
+        int low = 0;
+
+        if(s.size()==0)
+        {
+            return 0;
+        }
+
+        for(int i = 0;i<s.size();i++)
+        {
+            if(mp.find(s[i])!=mp.end())
+            {
+                int pos = mp[s[i]];
+                while(low<=pos)
+                {
+                    mp.erase(s[low]);
+                    low++;
+                }
+                mp[s[i]] = i;
+
+            }
+            else
+            {
+                mp[s[i]] = i;
+            }
+            maxi = max(maxi,int(mp.size()));
+
+        }
+        return maxi;
+        
+    }
+};
