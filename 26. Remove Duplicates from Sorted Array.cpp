09e4a71f5b182801627e8af17bd493eed9bd638d
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-08 21:20:40 +0530

    26. Remove Duplicates from Sorted Array
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>s;
+
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        nums.clear();
+        for(auto it:s){
+            nums.push_back(it);
+        }
+        return s.size();
+    }
+};
