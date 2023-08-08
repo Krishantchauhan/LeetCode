6a4930088876f808ca85d31ee34c46dd23f824f8
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-08 21:21:05 +0530

    27. Remove Element
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ct=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==val){
+
+        //     }
+                
+        // }
+        int high=0;
+        auto low = nums.begin();
+        while(high<nums.size()){
+            if(nums[high]==val){
+                nums.erase(low);
+                low--;
+                high--;
+            }
+            low++;
+            high++;
+        }
+        return nums.size();
+    }
+};
