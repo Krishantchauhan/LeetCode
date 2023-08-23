a3b4898be0fdade6a70568246019c398373f54b9
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-23 20:20:47 +0530

    ..704
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 704. Binary Search.cpp
+++ 704. Binary Search.cpp
@@ -1,16 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int> &nums, int target)
+    {
         int low = 0, high = nums.size() - 1;
         while (low <= high)
         {
             int mid = (low + high) / 2;
             if (target == nums[mid])
-            return mid ;
+                return mid;
             else if (nums[mid] > target)
-            high = mid - 1;
+                high = mid - 1;
             else
-            low = mid + 1;
+                low = mid + 1;
         }
         return -1;
     }
