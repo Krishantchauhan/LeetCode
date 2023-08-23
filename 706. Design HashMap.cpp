0b07ef81ac54b292a39c3339d5370a137bde9f74
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-23 20:19:04 +0530

    706. Design HashMap
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 706. Design HashMap.cpp
@@ -0,0 +1,32 @@
+class MyHashMap {
+public:
+
+    int nums[1000001];
+    MyHashMap() {
+        for(int i=0;i<1000001;i++)
+        nums[i]=-1;
+    }
+    
+    void put(int key, int value) {
+        nums[key]=value;
+    }
+    
+    int get(int key) {
+        if(nums[key]==-1)
+            return -1;
+        return nums[key];
+    }
+    
+    void remove(int key) {
+        if(nums[key]!=-1)
+            nums[key]=-1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
