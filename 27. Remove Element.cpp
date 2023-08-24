ac327acf53cae3feb1f3a91eadd85d3120ce00b3
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-24 20:06:35 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -1,17 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int ct=0;
+    int removeElement(vector<int> &nums, int val)
+    {
+        int ct = 0;
         // for(int i=0;i<nums.size();i++){
         //     if(nums[i]==val){
 
         //     }
-                
+
         // }
-        int high=0;
+        int high = 0;
         auto low = nums.begin();
-        while(high<nums.size()){
-            if(nums[high]==val){
+        while (high < nums.size())
+        {
+            if (nums[high] == val)
+            {
                 nums.erase(low);
                 low--;
                 high--;
