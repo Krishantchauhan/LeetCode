7162d9031533a7f0d71f6980884e7830033c92b4
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-24 20:06:44 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 31-Next-Permutation.cpp
+++ 31-Next-Permutation.cpp
@@ -1,20 +1,24 @@
-class Solution {
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
+        {
             i--;
         }
 
-        int j=n-1;
-        if(i>=0){
-            while(j>i && nums[i]>=nums[j])
+        int j = n - 1;
+        if (i >= 0)
+        {
+            while (j > i && nums[i] >= nums[j])
                 j--;
-            
-            swap(nums[i],nums[j]);
+
+            swap(nums[i], nums[j]);
         }
-        
-        reverse(nums.begin()+i+1,nums.end());
+
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
