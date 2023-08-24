8148fc96f7e8d7bb6f236a539ef9b678d7f644ff
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-24 20:07:32 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,14 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    int climbStairs(int n) {
-        if(n<=2)
+    int climbStairs(int n)
+    {
+        if (n <= 2)
             return n;
-        vector<int> ans(n+1);
-        ans[0]=0;
-        ans[1]=1;
-        ans[2]=2;
-        for(int i=3;i<=n;i++)
-            ans[i]=ans[i-1]+ans[i-2];
+        vector<int> ans(n + 1);
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 2;
+        for (int i = 3; i <= n; i++)
+            ans[i] = ans[i - 1] + ans[i - 2];
         return ans[n];
     }
 };
