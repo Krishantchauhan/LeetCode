4331ca44e59d69e619e462d5cb7b0eed3e9a344f
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:57:52 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -1,15 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        stack<char>st;
+    bool isValid(string s)
+    {
+        stack<char> st;
 
-        for(auto i:s){
-            if(st.empty()||i=='('||i=='{'||i=='[')
+        for (auto i : s)
+        {
+            if (st.empty() || i == '(' || i == '{' || i == '[')
                 st.push(i);
-            else{
-                if((i==')' && st.top()!='(')||
-                    (i==']' && st.top()!='[')||
-                    (i=='}' && st.top()!='{'))
+            else
+            {
+                if ((i == ')' && st.top() != '(') ||
+                    (i == ']' && st.top() != '[') ||
+                    (i == '}' && st.top() != '{'))
                     return false;
                 st.pop();
             }
