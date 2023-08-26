32ea4fd1125e3c9fa7d6d0f1a36ba93238e54736
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:57:16 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 11-Container-With-Most-Water.cpp
+++ 11-Container-With-Most-Water.cpp
@@ -1,18 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
-        int low=0,high=height.size()-1;
-        int mx=0;
+    int maxArea(vector<int> &height)
+    {
+        int low = 0, high = height.size() - 1;
+        int mx = 0;
 
-        while(low<high){
-            int low_height=height[low];
-            int end_height=height[high];
+        while (low < high)
+        {
+            int low_height = height[low];
+            int end_height = height[high];
 
-            int min_height=min(low_height,end_height);
-            int area = min_height * (high-low);
-            mx=max(area,mx);
+            int min_height = min(low_height, end_height);
+            int area = min_height * (high - low);
+            mx = max(area, mx);
 
-            if(low_height<end_height)
+            if (low_height < end_height)
                 low++;
             else
                 high--;
