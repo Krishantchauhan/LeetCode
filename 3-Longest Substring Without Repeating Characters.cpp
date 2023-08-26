40b20165cf22860bbde5b258ccc8bfe72864bc04
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:56:52 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 3-Longest Substring Without Repeating Characters.cpp
+++ 3-Longest Substring Without Repeating Characters.cpp
@@ -1,38 +1,36 @@
-class Solution {
+class Solution
+{
 public:
     int lengthOfLongestSubstring(string s)
     {
-        unordered_map<char,int> mp;
+        unordered_map<char, int> mp;
 
         int maxi = 1;
         int low = 0;
 
-        if(s.size()==0)
+        if (s.size() == 0)
         {
             return 0;
         }
 
-        for(int i = 0;i<s.size();i++)
+        for (int i = 0; i < s.size(); i++)
         {
-            if(mp.find(s[i])!=mp.end())
+            if (mp.find(s[i]) != mp.end())
             {
                 int pos = mp[s[i]];
-                while(low<=pos)
+                while (low <= pos)
                 {
                     mp.erase(s[low]);
                     low++;
                 }
                 mp[s[i]] = i;
-
             }
             else
             {
                 mp[s[i]] = i;
             }
-            maxi = max(maxi,int(mp.size()));
-
+            maxi = max(maxi, int(mp.size()));
         }
         return maxi;
-        
     }
 };
