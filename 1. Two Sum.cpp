be4b4e370bd394498fa078adb059b94630063b1a
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:56:27 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,11 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         unordered_map<int, int> mp;
-       
-        for(int i = 0; i < nums.size(); i++){
-            int diff=target - nums[i];
-            if(mp.find(diff) == mp.end())
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+            if (mp.find(diff) == mp.end())
                 mp[nums[i]] = i;
             else
                 return {mp[diff], i};
