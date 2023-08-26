6256f53af5af2ee3b7889d80502e08a5751d81e4
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:58:22 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 23-Merge-k-Sorted-Lists.cpp
+++ 23-Merge-k-Sorted-Lists.cpp
@@ -1,25 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n=lists.size(),i=0;
-        vector<int>v;
-        while(i<n){
-            ListNode *temp=lists[i];
-            while(temp!=NULL){
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        int n = lists.size(), i = 0;
+        vector<int> v;
+        while (i < n)
+        {
+            ListNode *temp = lists[i];
+            while (temp != NULL)
+            {
                 v.push_back(temp->val);
-                temp=temp->next;
+                temp = temp->next;
             }
             i++;
         }
 
-        sort(v.begin(),v.end());
+        sort(v.begin(), v.end());
 
         ListNode *nn = new ListNode(0);
-        ListNode *head=nn;
+        ListNode *head = nn;
 
-        for(int i=0;i<v.size();i++){
-            nn->next=new ListNode(v[i]);
-            nn=nn->next;
+        for (int i = 0; i < v.size(); i++)
+        {
+            nn->next = new ListNode(v[i]);
+            nn = nn->next;
         }
         return head->next;
     }
