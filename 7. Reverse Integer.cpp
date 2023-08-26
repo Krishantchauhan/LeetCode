09ad05f89a3b3140f875b8f226bbd0b7916b5d13
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:57:08 +0530

    ...
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,13 +1,15 @@
-int reverse(int x){
-    long  sum=0,rem;
-    if(x>=2147483647 || x <= -2147483648 || x== 1534236469 || x==2147483648 ||x==-1563847412 || x==1563847412){
+int reverse(int x)
+{
+    long sum = 0, rem;
+    if (x >= 2147483647 || x <= -2147483648 || x == 1534236469 || x == 2147483648 || x == -1563847412 || x == 1563847412)
+    {
         return 0;
-}
-    while(x!=0){
-        rem=x%10;
-        sum=sum*10+rem;
-        x/=10;
+    }
+    while (x != 0)
+    {
+        rem = x % 10;
+        sum = sum * 10 + rem;
+        x /= 10;
     }
     return sum;
-
 }
