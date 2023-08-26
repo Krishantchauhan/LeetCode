37620a9a04c8da0fee1bea0be707001f6fe576ec
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-26 20:56:36 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2-Add-Two-Numbers.cpp
+++ 2-Add-Two-Numbers.cpp
@@ -1,23 +1,28 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* NewList= new ListNode(0);
-        ListNode* curr = NewList;
-        int carry=0;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *NewList = new ListNode(0);
+        ListNode *curr = NewList;
+        int carry = 0;
 
-        while(l1!=NULL || l2!=NULL || carry>0){
-            int sum=carry;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
+        while (l1 != NULL || l2 != NULL || carry > 0)
+        {
+            int sum = carry;
+            if (l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
+            if (l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
             }
-            carry=sum/10;
-            curr->next = new ListNode(sum%10);
-            curr=curr->next;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
         }
 
         return NewList->next;
