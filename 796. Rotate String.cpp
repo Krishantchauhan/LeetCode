8bb57aec2c0334c07862f3e4945ae99dfe1836e6
Krishantchauhan <krashantchauhan@gmail.com>
2023-08-29 23:58:16 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 796. Rotate String.cpp
+++ 796. Rotate String.cpp
@@ -1,16 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    bool rotateString(string s, string goal) {
-        
-        if (s.size() != goal.size()) {
+    bool rotateString(string s, string goal)
+    {
+
+        if (s.size() != goal.size())
+        {
             return false;
         }
 
         deque<char> dq1(s.begin(), s.end());
         deque<char> dq2(goal.begin(), goal.end());
 
-        for (int i = 0; i < s.size(); i++) {
-            if (dq1 == dq2) {
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (dq1 == dq2)
+            {
                 return true;
             }
             dq1.push_back(dq1.front());
