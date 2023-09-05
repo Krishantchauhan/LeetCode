08128b08cadb2dd39e76083daa9a5b14dbd4f082
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-05 06:23:27 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2390-Removing Stars From a String.cpp
+++ 2390-Removing Stars From a String.cpp
@@ -1,9 +1,12 @@
-class Solution {
+class Solution
+{
 public:
-    string removeStars(string s) {
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='*')
+    string removeStars(string s)
+    {
+        string ans = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '*')
                 ans.pop_back();
             else
                 ans.push_back(s[i]);
