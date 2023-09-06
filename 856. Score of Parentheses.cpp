e8b5d987b606732507f53a3b45cdba0b43b6c4ab
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-06 21:22:18 +0530

    856. Score of Parentheses
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 856. Score of Parentheses.cpp
+++ 856. Score of Parentheses.cpp
@@ -1,16 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int scoreOfParentheses(string s) {
-        int ct=0;
-        stack<int>st;
+    int scoreOfParentheses(string s)
+    {
+        int ct = 0;
+        stack<int> st;
 
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '(')
+            {
                 st.push(ct);
-                ct=0;
+                ct = 0;
             }
-            else {
-                ct=st.top()+max(2*ct,1);
+            else
+            {
+                ct = st.top() + max(2 * ct, 1);
                 st.pop();
             }
             // else if
