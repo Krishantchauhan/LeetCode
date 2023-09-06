0493c1bbd2c34d889892738f007f9c72d8f4092a
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-06 21:22:44 +0530

    867. Transpose Matrix
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 867. Transpose Matrix.cpp
+++ 867. Transpose Matrix.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int r =matrix.size();
-        int c =matrix[0].size();
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        int r = matrix.size();
+        int c = matrix[0].size();
 
         vector<vector<int>> trans(c, vector<int>(r));
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
                 trans[j][i] = matrix[i][j];
             }
-        }        
+        }
         return trans;
     }
 };
