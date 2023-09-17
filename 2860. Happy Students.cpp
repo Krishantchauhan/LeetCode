9bef97b3ace2d07f729bd97ec7df275cec7ea9dc
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-17 21:30:20 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2860. Happy Students.cpp
+++ 2860. Happy Students.cpp
@@ -1,11 +1,13 @@
-class Solution {
+class Solution
+{
 public:
-    int countWays(vector<int>& nums) {
-        int ct=0;
-        sort(nums.begin(),nums.end());
-        
-        int n=nums.size();
-        
+    int countWays(vector<int> &nums)
+    {
+        int ct = 0;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
         // for(int i=0;i<n;i++){
         //     if( (nums[i]< i+1 ) && ((i+1<n) && (nums[i+1]>i+1)))
         //         ct++;
@@ -16,16 +18,17 @@ public:
         //     ct++;
         // return ct;
 
-        if(0<nums[0])
+        if (0 < nums[0])
             ct++;
-        for(int i=0;i<n-1;i++){
-            if(i>=nums[i] && i+1<nums[i+1])
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (i >= nums[i] && i + 1 < nums[i + 1])
                 ct++;
         }
 
-        if(n-1>=nums[n-1])
+        if (n - 1 >= nums[n - 1])
             ct++;
-        
+
         return ct;
     }
 };
