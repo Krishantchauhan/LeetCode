bbb5db87939934b39485956f4fa3af93b26a2605
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-18 18:31:20 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 326. Power of Three.cpp
+++ 326. Power of Three.cpp
@@ -1,10 +1,12 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPowerOfThree(int n) {
-        if(n<=0)
+    bool isPowerOfThree(int n)
+    {
+        if (n <= 0)
             return false;
-        while(n%3==0)
-            n/=3;
-        return n==1;
+        while (n % 3 == 0)
+            n /= 3;
+        return n == 1;
     }
 };
