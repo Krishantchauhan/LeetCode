3e8020932f81717c34500522719759d20e5f0aa6
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-18 18:30:47 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
+++ 2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -1,24 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int minimumRecolors(string str, int k) {
-        
+    int minimumRecolors(string str, int k)
+    {
+
         int n = str.size();
         int mx = INT_MIN;
-        int ct = 0;     
+        int ct = 0;
 
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(i < k)
+            if (i < k)
             {
-                if(str[i] == 'B')
+                if (str[i] == 'B')
                     ct++;
-            }            
+            }
             else
             {
-                mx = max(mx, ct);       
-                if(str[i - k] == 'B')
+                mx = max(mx, ct);
+                if (str[i - k] == 'B')
                     ct--;
-                if(str[i] == 'B')
+                if (str[i] == 'B')
                     ct++;
             }
         }
