715e4159fd1c53d8d2abebc1f35198c288c37292
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-18 18:29:12 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2861. Maximum Number of Alloys.cpp
+++ 2861. Maximum Number of Alloys.cpp
@@ -1,29 +1,36 @@
-class Solution {
+class Solution
+{
 public:
-    
-    bool calc(int i, int alloy, long long budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
-        
-        for (int j = 0; j < composition[i].size(); j++) {
+    bool calc(int i, int alloy, long long budget, vector<vector<int>> &composition, vector<int> &stock, vector<int> &cost)
+    {
+
+        for (int j = 0; j < composition[i].size(); j++)
+        {
             long long metal = static_cast<long long>(composition[i][j]) * alloy;
-            if (metal > stock[j]) {
+            if (metal > stock[j])
+            {
                 budget -= (metal - stock[j]) * static_cast<long long>(cost[j]);
             }
-            if (budget < 0) 
+            if (budget < 0)
                 return false;
         }
         return true;
     }
 
-    int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
+    int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>> &composition, vector<int> &stock, vector<int> &cost)
+    {
         int low = 0, high = INT_MAX;
         int mx = 0;
 
-        while (low <= high) {
-            int mid = low+(high-low) / 2;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
             bool flag = false;
 
-            for (int i = 0; i < k; i++) {
-                if (calc(i, mid, budget, composition, stock, cost)) {
+            for (int i = 0; i < k; i++)
+            {
+                if (calc(i, mid, budget, composition, stock, cost))
+                {
                     mx = mid;
                     flag = true;
                     low = mid + 1;
@@ -31,7 +38,7 @@ public:
                 }
             }
 
-            if (!flag) 
+            if (!flag)
                 high = mid - 1;
         }
         return mx;
