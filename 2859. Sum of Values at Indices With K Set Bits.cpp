c886ac8758049036cdae599039696396de36ea21
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-18 18:30:04 +0530

    2859. Sum of Values at Indices With K Set Bits
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 2859. Sum of Values at Indices With K Set Bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    int calc(int num){
+        int ct=0;
+        while(num>0){
+            ct+=num&1;
+            num>>=1;
+        }
+        return ct;            
+    }
+    
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(calc(i)==k)
+                sum+=nums[i];
+        }
+        return sum;
+    }
+};
