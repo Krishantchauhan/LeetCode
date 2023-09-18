47bf94b50bb90bef99a8b883a3165abb4deb9931
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-18 18:28:28 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1672. Richest Customer Wealth.cpp
+++ 1672. Richest Customer Wealth.cpp
@@ -1,12 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    int maximumWealth(vector<vector<int>>& accounts) {
-        int mx=0;
-        for(int i=0;i<accounts.size();i++){
-            int sum=0;
-            for(int j=0;j<accounts[0].size();j++)
-                sum+=accounts[i][j];
-            mx=max(sum,mx);
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int mx = 0;
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < accounts[0].size(); j++)
+                sum += accounts[i][j];
+            mx = max(sum, mx);
         }
         return mx;
     }
