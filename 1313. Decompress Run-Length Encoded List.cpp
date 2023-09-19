21b717eee49bdd13772ed25382fdb77320451130
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-19 20:55:30 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1313. Decompress Run-Length Encoded List.cpp
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -1,12 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> decompressRLElist(vector<int>& nums) {
-        vector<int>ans;
-        for(int i=0; i<nums.size();i=i+2)
-        {           
-            int x=nums[i];
-            for(int j=0; j<x;j++)
-                ans.push_back(nums[i+1]);
+    vector<int> decompressRLElist(vector<int> &nums)
+    {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i = i + 2)
+        {
+            int x = nums[i];
+            for (int j = 0; j < x; j++)
+                ans.push_back(nums[i + 1]);
         }
         return ans;
     }
