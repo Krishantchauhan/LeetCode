ed98583c0385588a459030cf496fb05b28e63a4e
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-19 20:54:20 +0530

    2856. Minimum Array Length After Pair Removals
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 2856. Minimum Array Length After Pair Removals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        priority_queue<int> pq;
+        
+        for(auto it:nums) 
+          mp[it]++;
+        
+        for(auto it : mp) 
+          pq.push(it.second);
+        // cout<<pq.size();
+         while(!pq.empty())
+        {
+            if(pq.size() == 1) 
+              return pq.top(); 
+
+            int top1 = pq.top();
+            pq.pop();
+            int top2 = pq.top(); 
+            pq.pop();
+             
+            
+            if(top1 > 1) 
+                pq.push(top1 - 1);
+            if(top2 > 1) 
+                pq.push(top2 - 1);
+        }
+      return 0;
+    }
+};
