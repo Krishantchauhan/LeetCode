681721669309898b574ca991cdb5605f713497d4
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-19 20:53:52 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 2855. Minimum Right Shifts to Sort the Array.cpp
+++ 2855. Minimum Right Shifts to Sort the Array.cpp
@@ -1,22 +1,28 @@
-class Solution {
+class Solution
+{
 public:
-    int minimumRightShifts(vector<int>& nums) {
-        int  n = nums.size();
+    int minimumRightShifts(vector<int> &nums)
+    {
+        int n = nums.size();
         int idx = -1;
         int ct{};
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                if(ct==0){
-                    idx = i+1;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                if (ct == 0)
+                {
+                    idx = i + 1;
                     ct++;
-                }else 
+                }
+                else
                     return -1;
             }
         }
-        if(idx==-1) 
+        if (idx == -1)
             return 0;
-        if(nums[n-1]>nums[0]) 
+        if (nums[n - 1] > nums[0])
             return -1;
-        return n-idx;
+        return n - idx;
     }
 };
