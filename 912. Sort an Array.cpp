97b3905b6323589b53a7615d8e40658df9bb7505
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-20 20:28:10 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 912. Sort an Array.cpp
+++ 912. Sort an Array.cpp
@@ -1,15 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        map<int,int>mp;
+    vector<int> sortArray(vector<int> &nums)
+    {
+        map<int, int> mp;
 
-        for(int i=0;i<nums.size();i++){
+        for (int i = 0; i < nums.size(); i++)
+        {
             mp[nums[i]]++;
         }
-        int k=0;
-        for(auto it:mp){
-            while(it.second>0){
-                nums[k]=it.first;
+        int k = 0;
+        for (auto it : mp)
+        {
+            while (it.second > 0)
+            {
+                nums[k] = it.first;
                 k++;
                 it.second--;
             }
