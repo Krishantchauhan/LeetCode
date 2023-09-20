621fcf1dd027ef208a0cedb0bbb2b767772cb005
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-20 20:26:47 +0530

    901 online stock span
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 901. Online Stock Span.cpp
@@ -0,0 +1,20 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    
+    stack<pair<int,int>>st;
+
+    int next(int price) {
+        int ct=1;
+
+        while(st.size()>0 && st.top().first<=price){
+            ct+=st.top().second;
+            // cout<<st.top().first<<" "<<st.top().second<<endl;
+            st.pop();
+        }
+        st.push({price,ct});
+        return ct;
+    }
+};
